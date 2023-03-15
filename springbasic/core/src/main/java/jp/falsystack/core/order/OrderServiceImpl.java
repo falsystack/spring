94b6a2b6de246b81bcd17f329ed7f768907d93cd
falsystack <falsystack@gmail.com>
2023-03-15 23:19:03 +0900

    feat(spring-basic-core): component scan

--- springbasic/core/src/main/java/jp/falsystack/core/order/OrderServiceImpl.java
+++ springbasic/core/src/main/java/jp/falsystack/core/order/OrderServiceImpl.java
@@ -3,20 +3,15 @@ package jp.falsystack.core.order;
 import jp.falsystack.core.discount.DiscountPolicy;
 import jp.falsystack.core.member.Member;
 import jp.falsystack.core.member.MemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
   private final MemberRepository memberRepository;
   private final DiscountPolicy discountPolicy;
-
-  @Autowired
-  public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-    this.memberRepository = memberRepository;
-    this.discountPolicy = discountPolicy;
-  }
 
   @Override
   public Order createOrder(Long memberId, String itemName, int itemPrice) {
