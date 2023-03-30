2084d84bb7968157955f2577864f7803e4ddd270
falsystack <falsystack@gmail.com>
2023-03-30 23:44:09 +0900

    feat(srping-mvc-01): request mapping

--- springmvc01/springmvc/src/main/java/jp/falsystack/springmvc/basic/LogTestController.java
+++ springmvc01/springmvc/src/main/java/jp/falsystack/springmvc/basic/LogTestController.java
@@ -1,19 +1,21 @@
 package jp.falsystack.springmvc.basic;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class LogTestController {
 
-  private final Logger log = LoggerFactory.getLogger(getClass());
+//  @Slf4j 로 대체
+//  private final Logger log = LoggerFactory.getLogger(getClass());
 
   @RequestMapping("/log-test")
   public String logTest() {
     String name = "Spring";
     log.info("name = {} ", name);
+    log.debug("name = {} ", name);
 
     return "ok";
   }
---
+++ springmvc01/springmvc/src/main/java/jp/falsystack/springmvc/basic/requestmapping/MappingController.java
@@ -0,0 +1,2 @@
+package jp.falsystack.springmvc.basic.requestmapping;public class MappingController {
+}
