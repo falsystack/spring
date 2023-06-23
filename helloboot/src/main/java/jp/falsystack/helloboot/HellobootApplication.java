165da5662bffa6c01edf0c5160e714a83261dae1
falsystack <falsystack@gmail.com>
2023-06-24 00:33:10 +0900

    feat(helloboot): Use embedded tomcat

--- helloboot/src/main/java/jp/falsystack/helloboot/HellobootApplication.java
+++ helloboot/src/main/java/jp/falsystack/helloboot/HellobootApplication.java
@@ -1,13 +1,14 @@
 package jp.falsystack.helloboot;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
 
-@SpringBootApplication
 public class HellobootApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(HellobootApplication.class, args);
+        TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+        WebServer webServer = serverFactory.getWebServer(); // tomcat 이외에도 지원가능하도록 추상화되어있다.
+        webServer.start();
     }
 
 }
