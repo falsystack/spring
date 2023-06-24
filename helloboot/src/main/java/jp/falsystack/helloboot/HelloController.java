96a00c2354bbc12e6aed65d99bae41898ea73b07
falsystack <falsystack@gmail.com>
2023-06-24 22:36:26 +0900

    feat(helloboot): Use annotation mapping

--- helloboot/src/main/java/jp/falsystack/helloboot/HelloController.java
+++ helloboot/src/main/java/jp/falsystack/helloboot/HelloController.java
@@ -1,16 +1,22 @@
 package jp.falsystack.helloboot;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import java.util.Objects;
 
+@RequestMapping("/hello")
 public class HelloController {
 
     private final HelloService helloService;
 
-    // 스프링은 container에 등록된 빈들중에 주입되야 할 HelloService가 있는지 찾는다.
     public HelloController(HelloService helloService) {
         this.helloService = helloService;
     }
 
+    @GetMapping
+    @ResponseBody
     public String hello(String name) {
         return helloService.sayHello(Objects.requireNonNull(name));
     }
