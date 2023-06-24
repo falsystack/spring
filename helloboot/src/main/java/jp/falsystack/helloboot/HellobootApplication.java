f8fb39c0183ddc65f228708d7cdbc8cf127f4f54
falsystack <falsystack@gmail.com>
2023-06-24 21:05:58 +0900

    feat(helloboot): Front controller

--- helloboot/src/main/java/jp/falsystack/helloboot/HellobootApplication.java
+++ helloboot/src/main/java/jp/falsystack/helloboot/HellobootApplication.java
@@ -2,12 +2,47 @@ package jp.falsystack.helloboot;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class HellobootApplication {
 
     public static void main(String[] args) {
         TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-        WebServer webServer = serverFactory.getWebServer(); // tomcat 이외에도 지원가능하도록 추상화되어있다.
+        WebServer webServer = serverFactory.getWebServer(servletContext -> {
+            HelloController helloController = new HelloController();
+
+            servletContext.addServlet("frontController", new HttpServlet() {
+                @Override
+                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                    // 인증, 보안, 다국어, 공통 기능 등등
+
+                    // GET, "/hello"
+                    if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
+                        String name = req.getParameter("name");
+
+                        // logic의 분리
+                        String ret = helloController.hello(name);
+
+                        resp.setStatus(HttpStatus.OK.value()); // 상태코드
+                        resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE); // 헤더
+                        resp.getWriter().println(ret); // 바디
+                    } else if (req.getRequestURI().equals("/user")) {
+                        //
+                    } else {
+                        resp.setStatus(HttpStatus.NOT_FOUND.value());
+                    }
+                }
+            }).addMapping("/*"); // 모든 요청을 다 받는다.
+        });
         webServer.start();
     }
 
