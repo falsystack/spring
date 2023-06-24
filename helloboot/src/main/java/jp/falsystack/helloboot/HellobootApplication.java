41702d2ad47a8f83a09cb445ed7bc0cbf377b65f
falsystack <falsystack@gmail.com>
2023-06-25 00:18:10 +0900

    feat(helloboot): Create MySpringApplication

--- helloboot/src/main/java/jp/falsystack/helloboot/HellobootApplication.java
+++ helloboot/src/main/java/jp/falsystack/helloboot/HellobootApplication.java
@@ -1,32 +1,28 @@
 package jp.falsystack.helloboot;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
+
 
 @ComponentScan
 @Configuration
 public class HellobootApplication {
-    public static void main(String[] args) {
-        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
-            @Override
-            protected void onRefresh() {
-                super.onRefresh();
 
-                TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-                WebServer webServer = serverFactory.getWebServer(servletContext -> {
-                    servletContext.addServlet("dispatcherServlet",
-                                    new DispatcherServlet(this))
-                            .addMapping("/*");
-                });
-                webServer.start();
-            }
-        };
-        applicationContext.register(HellobootApplication.class); // 설정정보 등록
-        applicationContext.refresh();
+    @Bean
+    public ServletWebServerFactory servletWebServerFactory() {
+        return new TomcatServletWebServerFactory();
     }
 
+    @Bean
+    public DispatcherServlet dispatcherServlet() {
+        return new DispatcherServlet();
+    }
+
+    public static void main(String[] args) {
+        MySpringApplication.run(HellobootApplication.class, args);
+    }
 }
