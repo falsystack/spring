2fb4b0b9cedd80168f2c1cc226def95929adb3b4
falsystack <falsystack@gmail.com>
2023-07-01 10:12:53 +0900

    test(core): create rate discount policy test

---
+++ core/src/main/java/jp/falsystack/core/discount/RateDiscountPolicy.java
@@ -0,0 +1,17 @@
+package jp.falsystack.core.discount;
+
+import jp.falsystack.core.member.Grade;
+import jp.falsystack.core.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy {
+
+    private int DISCOUNT_RATE = 10;
+
+    @Override
+    public int discount(Member member, int price) {
+        if (member.getGrade() == Grade.VIP) {
+            return price * DISCOUNT_RATE / 100;
+        }
+        return 0;
+    }
+}
