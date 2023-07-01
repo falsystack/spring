e5432e6d57e7fc82961f6c9212e49c4c73eef62d
falsystack <falsystack@gmail.com>
2023-07-01 09:29:05 +0900

    test(core): create order service test

--- core/src/test/java/jp/falsystack/core/order/OrderServiceTest.java
+++ core/src/test/java/jp/falsystack/core/order/OrderServiceTest.java
@@ -1,14 +1,28 @@
 package jp.falsystack.core.order;
 
+import jp.falsystack.core.member.Grade;
+import jp.falsystack.core.member.Member;
+import jp.falsystack.core.member.MemberService;
+import jp.falsystack.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class OrderServiceTest {
 
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
     @Test
     void createOrder() {
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
 
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
     }
 
 }
