8a397aab5e8b7e3b6fcd8d39cf7d4724d7542da2
falsystack <falsystack@gmail.com>
2023-07-01 13:01:14 +0900

    refactor(core): AppConfig Refactoring

--- core/src/main/java/jp/falsystack/core/AppConfig.java
+++ core/src/main/java/jp/falsystack/core/AppConfig.java
@@ -1,6 +1,9 @@
 package jp.falsystack.core;
 
+import jp.falsystack.core.discount.DiscountPolicy;
 import jp.falsystack.core.discount.FixDiscountPolicy;
+import jp.falsystack.core.discount.RateDiscountPolicy;
+import jp.falsystack.core.member.MemberRepository;
 import jp.falsystack.core.member.MemberService;
 import jp.falsystack.core.member.MemberServiceImpl;
 import jp.falsystack.core.member.MemoryMemberRepository;
@@ -9,11 +12,21 @@ import jp.falsystack.core.order.OrderServiceImpl;
 
 public class AppConfig {
 
+    public DiscountPolicy discountPolicy() {
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
+    }
+
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 }
