ce312ddebb46aad0498fdb0df4b87c38730cf6e1
falsystack <falsystack@gmail.com>
2023-07-16 15:23:05 +0900

    feat(servlet): http request - get, query-param

--- servlet/src/main/java/jp/falsystack/servlet/basic/request/RequestHeaderServlet.java
+++ servlet/src/main/java/jp/falsystack/servlet/basic/request/RequestHeaderServlet.java
@@ -1,7 +1,5 @@
 package jp.falsystack.servlet.basic.request;
 
-import org.springframework.util.StreamUtils;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.Cookie;
@@ -9,7 +7,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Enumeration;
 
 @WebServlet(name = "requestHeaderServlet", urlPatterns = "/request-header")
 public class RequestHeaderServlet extends HttpServlet {
