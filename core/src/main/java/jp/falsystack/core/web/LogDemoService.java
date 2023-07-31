ca5d35e4b9b0c552422fb4e1914f24eddd8b37d7
falsystack <falsystack@gmail.com>
2023-08-01 07:06:21 +0900

    feat(springmvc): request param

--- core/src/main/java/jp/falsystack/core/web/LogDemoService.java
+++ core/src/main/java/jp/falsystack/core/web/LogDemoService.java
@@ -1,2 +1,16 @@
-package jp.falsystack.core.web;public class LogDemoService {
+package jp.falsystack.core.web;
+
+import jp.falsystack.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LogDemoService {
+
+    private final MyLogger myLogger;
+
+    public void logic(String id) {
+        myLogger.log("service id = " + id);
+    }
 }
---
+++ springmvc/src/main/java/jp/falsystack/springmvc/basic/HelloData.java
@@ -0,0 +1,9 @@
+package jp.falsystack.springmvc.basic;
+
+import lombok.Data;
+
+@Data
+public class HelloData {
+    private String username;
+    private int age;
+}
