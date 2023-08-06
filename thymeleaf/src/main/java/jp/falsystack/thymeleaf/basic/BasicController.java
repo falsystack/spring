2f9473e99c7ddd068e0c7397b1a5879c2b06fd83
falsystack <falsystack@gmail.com>
2023-08-06 17:24:46 +0900

    feat(springmvc2): date util & link

--- thymeleaf/src/main/java/jp/falsystack/thymeleaf/basic/BasicController.java
+++ thymeleaf/src/main/java/jp/falsystack/thymeleaf/basic/BasicController.java
@@ -11,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
 
@@ -60,6 +61,19 @@ public class BasicController {
         model.addAttribute("servletContext", request.getServletContext());
 
         return "/basic/basic-objects";
+    }
+
+    @GetMapping("/date")
+    public String date(Model model) {
+        model.addAttribute("localDateTime", LocalDateTime.now());
+        return "basic/date";
+    }
+
+    @GetMapping("/link")
+    public String link(Model model) {
+        model.addAttribute("param1", "data1");
+        model.addAttribute("param2", "data2");
+        return "/basic/link";
     }
 
     @Component("helloBean")
