8aeab946a92e63b496de2c8a11bba2b1453f25ce
falsystack <falsystack@gmail.com>
2023-08-06 16:34:20 +0900

    feat(springmvc2): spring el & th:with

--- thymeleaf/src/main/java/jp/falsystack/thymeleaf/basic/BasicController.java
+++ thymeleaf/src/main/java/jp/falsystack/thymeleaf/basic/BasicController.java
@@ -1,9 +1,14 @@
 package jp.falsystack.thymeleaf.basic;
 
+import lombok.Builder;
+import lombok.Data;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/basic")
@@ -18,6 +23,40 @@ public class BasicController {
     @GetMapping("text-unescaped")
     public String textUnescaped(Model model) {
         model.addAttribute("data", "Hello <b>Spring</b>");
-        return "/basic/text-unescaped.html";
+        return "/basic/text-unescaped";
+    }
+
+    @GetMapping("/variable")
+    public String variable(Model model) {
+        User userA = User.builder()
+                .username("userA")
+                .age(20)
+                .build();
+
+        User userB = User.builder()
+                .username("userB")
+                .age(30)
+                .build();
+
+        List<User> list = List.of(userA, userB);
+        Map<String, User> map = Map.of("userA", userA, "userB", userB);
+
+        model.addAttribute("user", userA);
+        model.addAttribute("users", list);
+        model.addAttribute("userMap", map);
+
+        return "/basic/variable";
+    }
+
+    @Data
+    static class User {
+        private String username;
+        private int age;
+
+        @Builder
+        public User(String username, int age) {
+            this.username = username;
+            this.age = age;
+        }
     }
 }
