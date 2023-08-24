258c43373e4ea2c1012b64a700764509d509732e
falsystack <falsystack@gmail.com>
2023-08-24 21:38:46 +0900

    feat: servlet - HttpSession

--- login/src/main/java/hello/login/web/login/LoginController.java
+++ login/src/main/java/hello/login/web/login/LoginController.java
@@ -2,6 +2,7 @@ package hello.login.web.login;
 
 import hello.login.domain.login.LoginService;
 import hello.login.domain.member.Member;
+import hello.login.web.SessionConst;
 import hello.login.web.session.SessionManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
 @Slf4j
@@ -51,7 +53,7 @@ public class LoginController {
         return "redirect:/";
     }
 
-    @PostMapping("login")
+//    @PostMapping("login")
     public String loginV2(@Valid @ModelAttribute LoginForm form,
                           BindingResult bindingResult,
                           HttpServletResponse response) {
@@ -71,6 +73,27 @@ public class LoginController {
         return "redirect:/";
     }
 
+    @PostMapping("login")
+    public String loginV3(@Valid @ModelAttribute LoginForm form,
+                          BindingResult bindingResult,
+                          HttpServletRequest request) {
+        if (bindingResult.hasErrors()) {
+            return "login/loginForm";
+        }
+
+        Member member = loginService.login(form.getLoginId(), form.getPassword());
+
+        if (member == null) {
+            bindingResult.reject("loginFail", "Wrong Id or Wrong Password");
+            return "login/loginForm";
+        }
+
+        HttpSession session = request.getSession(); // 없으면 새로 만들어서 반환, 있으면 있는것 반환
+        session.setAttribute(SessionConst.LOGIN_MEMBER, member);
+
+        return "redirect:/";
+    }
+
 
 //    @PostMapping("/logout")
     public String logout(HttpServletResponse response) {
@@ -81,9 +104,19 @@ public class LoginController {
         return "redirect:/";
     }
 
-    @PostMapping("/logout")
+//    @PostMapping("/logout")
     public String logoutV2(HttpServletRequest request) {
         sessionManager.expire(request);
+        return "redirect:/";
+    }
+
+    @PostMapping("/logout")
+    public String logoutV3(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            // 세션 삭제
+            session.invalidate();
+        }
         return "redirect:/";
     }
 }
