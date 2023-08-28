6a6c3fdc9fab940b5a0304ef2e0e9b9d3e6c0e4f
falsystack <falsystack@gmail.com>
2023-08-28 23:51:07 +0900

    feat: Spring Interceptor - logger

---
+++ login/src/main/java/hello/login/web/interceptor/LogInterceptor.java
@@ -0,0 +1,51 @@
+package hello.login.web.interceptor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.UUID;
+
+@Slf4j
+public class LogInterceptor implements HandlerInterceptor {
+
+    public static final String LOG_ID = "logId";
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String requestURI = request.getRequestURI();
+        String uuid = UUID.randomUUID().toString();
+
+        // Interceptor가 싱글톤으로 등록되기 때문에 공유자원(static 변수 등)을 사용해서는 안된다.
+        // 대신에 attribute에 넣어서 전달하자.
+        request.setAttribute(LOG_ID, uuid);
+
+        // @RequestMapping 관련은 HandlerMethod 에서 제공 한다.
+        // 정적 리소스 관련은 ResourceHttpRequestHandler 에서 제공 한다.
+        if (handler instanceof HandlerMethod) {
+            HandlerMethod hm = (HandlerMethod) handler; // 호출할 컨트롤러 메서드의 모든 정보가 포함되어 있다.
+        }
+
+        log.info("REQUEST  [{}][{}][{}]", uuid, requestURI, handler);
+
+        return true; //false 진행X
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        log.info("postHandle [{}]", modelAndView);
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        String requestURI = request.getRequestURI();
+        String logId = (String)request.getAttribute(LOG_ID);
+        log.info("RESPONSE [{}][{}]", logId, requestURI);
+        if (ex != null) {
+            log.error("afterCompletion error!!", ex);
+        }
+    }
+}
