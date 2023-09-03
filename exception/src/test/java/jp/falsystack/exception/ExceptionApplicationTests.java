48a860592f582ebdbbdfc8fe2117be42ed6f859f
falsystack <falsystack@gmail.com>
2023-09-03 14:28:32 +0900

    feat: typeconverter

---
+++ exception/src/test/java/jp/falsystack/exception/ExceptionApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.falsystack.exception;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExceptionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
