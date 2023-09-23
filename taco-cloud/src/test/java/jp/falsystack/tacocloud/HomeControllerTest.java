18a0d7a099016e851df671e000d7e0211f3b9756
falsystack <falsystack@gmail.com>
2023-09-23 15:03:16 +0900

    Spring in Action start

---
+++ taco-cloud/src/test/java/jp/falsystack/tacocloud/HomeControllerTest.java
@@ -0,0 +1,26 @@
+package jp.falsystack.tacocloud;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@WebMvcTest(HomeController.class)
+class HomeControllerTest {
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @Test
+  void testHomePage() throws Exception {
+    mockMvc.perform(MockMvcRequestBuilders.get("/"))
+        .andExpect(MockMvcResultMatchers.status().isOk())
+        .andExpect(MockMvcResultMatchers.view().name("home"))
+        .andExpect(
+            MockMvcResultMatchers.content().string(Matchers.containsString("Welcome to...")));
+  }
+
+}
