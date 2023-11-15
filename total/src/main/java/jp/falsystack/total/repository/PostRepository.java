ef4b480af80024060d5e43a23096deb84283adc5
falsystack <falsystack@gmail.com>
2023-11-15 21:27:19 +0900

    .

--- total/src/main/java/jp/falsystack/total/repository/PostRepository.java
+++
@@ -1,9 +0,0 @@
-package jp.falsystack.total.repository;
-
-import jp.falsystack.total.domain.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PostRepository extends JpaRepository<Post, Long> {
-}
