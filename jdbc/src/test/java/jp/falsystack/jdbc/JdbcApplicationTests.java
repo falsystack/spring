1f7bc192d8bba918839711efdd4ff5f9edeba15c
falsystack <falsystack@gmail.com>
2023-11-20 21:25:02 +0900

    jdbc project init

---
+++ jdbc/src/test/java/jp/falsystack/jdbc/JdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.falsystack.jdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
