97fa7f7a39fe7b1aeb8c2733f220b3942e443bda
falsystack <falsystack@gmail.com>
2024-02-16 00:30:50 +0900

    feat: ServletWebServerFactory

--- helloboot/src/main/java/jp/falsystack/helloboot/HellobootApplication.java
+++ helloboot/src/main/java/jp/falsystack/helloboot/HellobootApplication.java
@@ -2,11 +2,16 @@ package jp.falsystack.helloboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 
 public class HellobootApplication {
 
     public static void main(String[] args) {
-        System.out.println("Hello Containerless Standalone Application");
+        ServletWebServerFactory factory = new TomcatServletWebServerFactory();
+        WebServer webServer = factory.getWebServer();
+        webServer.start();
     }
 
 }
