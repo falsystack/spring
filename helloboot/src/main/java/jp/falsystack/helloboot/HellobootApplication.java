4c1f3146ac82f349eba48bae1d755621bc91ab2e
falsystack <falsystack@gmail.com>
2024-02-17 01:58:17 +0900

    feat: Hello Servlet

--- helloboot/src/main/java/jp/falsystack/helloboot/HellobootApplication.java
+++ helloboot/src/main/java/jp/falsystack/helloboot/HellobootApplication.java
@@ -1,16 +1,33 @@
 package jp.falsystack.helloboot;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+
+import java.io.IOException;
 
 public class HellobootApplication {
 
     public static void main(String[] args) {
         ServletWebServerFactory factory = new TomcatServletWebServerFactory();
-        WebServer webServer = factory.getWebServer();
+        WebServer webServer = factory.getWebServer(servletContext -> {
+            servletContext.addServlet("hello", new HttpServlet() {
+                @Override
+                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                    resp.setStatus(HttpStatus.OK.value());
+                    resp.setContentType("text/plain");
+                    resp.getWriter().write("Hello Servlet");
+                }
+            }).addMapping("/hello");
+        });
         webServer.start();
     }
 
