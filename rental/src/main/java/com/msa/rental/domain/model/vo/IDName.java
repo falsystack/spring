d87a39921335730bcc0fde5fa7096b9dc0fab55b
falsystack <falsystack@gmail.com>
2024-02-19 00:50:14 +0900

    add rental project

---
+++ rental/src/main/java/com/msa/rental/domain/model/vo/IDName.java
@@ -0,0 +1,15 @@
+package com.msa.rental.domain.model.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class IDName {
+
+    private String id;
+    private String name;
+
+}
