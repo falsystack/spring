7669bf92d798ac2a340c55dc6cde363f06ae8dc6
falsystack <falsystack@gmail.com>
2024-02-20 00:42:54 +0900

    feat: Create Eureka Server

---
+++ discoverservice/src/main/java/jp/falsystack/discoverservice/DiscoverserviceApplication.java
@@ -0,0 +1,15 @@
+package jp.falsystack.discoverservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class DiscoverserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoverserviceApplication.class, args);
+    }
+
+}
