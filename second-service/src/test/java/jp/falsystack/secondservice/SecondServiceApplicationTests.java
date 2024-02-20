9666aa96fba75096600d020c8a65358bc12847de
falsystack <falsystack@gmail.com>
2024-02-21 00:30:15 +0900

    feat(spring-msa): Spring Cloud Gateway

---
+++ second-service/src/test/java/jp/falsystack/secondservice/SecondServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.falsystack.secondservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
