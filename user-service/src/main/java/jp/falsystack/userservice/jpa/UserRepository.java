b9582e6a04d39a013a333d4ab22de847d7fd095e
falsystack <falsystack@gmail.com>
2024-02-20 09:59:17 +0900

    feat: Create User-Service and regist to Eureka Server

--- user-service/src/main/java/jp/falsystack/userservice/jpa/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package jp.falsystack.userservice.jpa;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
-    UserEntity findByUserId(String userId);
-}
