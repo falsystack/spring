51e8271d923ca6ebe0678170f74b6ab228cd550a
falsystack <falsystack@gmail.com>
2024-02-21 00:19:33 +0900

    feat(spring-msa): Remove old first service

--- gateway-service/src/test/java/jp/falsystack/gatewayservice/GatewayServiceApplicationTests.java
+++ gateway-service/src/test/java/jp/falsystack/gatewayservice/GatewayServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package jp.falsystack.firstservice;
+package jp.falsystack.gatewayservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class FirstServiceApplicationTests {
+class GatewayServiceApplicationTests {
 
     @Test
     void contextLoads() {
