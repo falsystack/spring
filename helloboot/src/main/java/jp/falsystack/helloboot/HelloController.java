e9f35b1bc500150449d6e3808a7c9b1ad55f4a1a
falsystack <falsystack@gmail.com>
2024-02-25 12:41:06 +0900

    feat(toby-spring): Component Scan

--- helloboot/src/main/java/jp/falsystack/helloboot/HelloController.java
+++ helloboot/src/main/java/jp/falsystack/helloboot/HelloController.java
@@ -1,22 +1,20 @@
 package jp.falsystack.helloboot;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Objects;
 
 @RequiredArgsConstructor
-@Controller
-@RequestMapping("/hello")
+@RestController
 public class HelloController {
 
     private final HelloService helloService;
 
     @ResponseBody
-    @GetMapping
+    @GetMapping("/hello")
     public String hello(String name) {
         return helloService.sayHello(Objects.requireNonNull(name));
     }
