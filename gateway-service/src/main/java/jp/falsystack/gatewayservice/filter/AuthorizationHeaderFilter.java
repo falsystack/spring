95650c52c383eb373987a059768e088c4d3ad3de
falsystack <falsystack@gmail.com>
2024-03-01 00:48:49 +0900

    feat(spring-msa): Add JWT to gateway

---
+++ gateway-service/src/main/java/jp/falsystack/gatewayservice/filter/AuthorizationHeaderFilter.java
@@ -0,0 +1,78 @@
+package jp.falsystack.gatewayservice.filter;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<AuthorizationHeaderFilter.Config> {
+
+    private final Environment env;
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return (ex, chain) -> {
+            ServerHttpRequest request = ex.getRequest();
+
+            if (!request.getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
+                return onError(ex, "No authorization header", HttpStatus.UNAUTHORIZED);
+            }
+
+            String authorizationHeader = request.getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
+            String jwt = authorizationHeader.replace("Bearer", "");
+
+            if (!isJwtValid(jwt)) {
+                return onError(ex, "JWT token is not valid", HttpStatus.UNAUTHORIZED);
+            }
+
+            return chain.filter(ex);
+        };
+    }
+
+    private Mono<Void> onError(ServerWebExchange exchange, String error, HttpStatus httpStatus) {
+        ServerHttpResponse response = exchange.getResponse();
+        response.setStatusCode(httpStatus);
+
+        log.error(error);
+        return response.setComplete();
+    }
+
+    private boolean isJwtValid(String jwt) {
+        boolean returnValue = true;
+
+        String subject = null;
+        Jws<Claims> claims = Jwts.parser()
+                .verifyWith(Keys.hmacShaKeyFor(Decoders.BASE64URL.decode(env.getProperty("token.secret"))))
+                .build()
+                .parseSignedClaims(jwt);
+
+        // jwt의 body에있는 subject 확인
+        subject = claims.getHeader().toString();
+        if (subject == null || subject.isEmpty()) {
+            returnValue = false;
+        }
+        String userId = claims.getBody().getSubject();
+
+        return returnValue;
+    }
+
+    public static class Config {
+
+    }
+}
