99369d93875be048fa8f5d25be5c3997e2f9f5cd
falsystack <falsystack@gmail.com>
2024-03-04 11:07:53 +0900

    test(toby-spring): Assertions

--- helloboot/src/test/java/jp/falsystack/helloboot/HelloApiTest.java
+++ helloboot/src/test/java/jp/falsystack/helloboot/HelloApiTest.java
@@ -2,7 +2,12 @@ package jp.falsystack.helloboot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class HelloApiTest {
 
@@ -12,6 +17,9 @@ public class HelloApiTest {
         ResponseEntity<String> resp =
                 rest.getForEntity("http://localhost:8080/hello?name={name}", String.class, "Spring");
 
+        assertThat(resp.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(resp.getHeaders().getFirst(HttpHeaders.CONTENT_TYPE)).startsWith(MediaType.TEXT_PLAIN_VALUE);
+        assertThat(resp.getBody()).isEqualTo("Hello Spring");
 
     }
 }
