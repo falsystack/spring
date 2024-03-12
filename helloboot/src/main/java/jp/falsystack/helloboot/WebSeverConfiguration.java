c34f4886ab9a6f9b89cbf853cc1c03b3770c7db5
尹健雄 <falsystack@gmail.com>
2024-03-13 00:29:00 +0900

    feat(toby-spring): Replace AutoConfiguration

---
+++ helloboot/src/main/java/jp/falsystack/helloboot/WebSeverConfiguration.java
@@ -0,0 +1,18 @@
+package jp.falsystack.helloboot;
+
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration(proxyBeanMethods = false)
+public class WebSeverConfiguration {
+
+    @Bean
+    ServletWebServerFactory customerWebServerFactory() {
+        TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+        serverFactory.setPort(9090);
+        return serverFactory;
+    }
+
+}
