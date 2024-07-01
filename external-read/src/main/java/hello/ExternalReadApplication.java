d7abd5f07bc9780f64660c976797a01d174c9fa2
falsystack <falsystack@gmail.com>
2024-07-02 00:03:52 +0900

    feat: @ConfigurationProperties with validation

--- external-read/src/main/java/hello/ExternalReadApplication.java
+++ external-read/src/main/java/hello/ExternalReadApplication.java
@@ -1,20 +1,18 @@
 package hello;
 
-import hello.config.MyDataSourceConfigV1;
-import hello.config.MyDataSourceConfigV2;
-import hello.config.MyDataSourceEnvConfig;
-import hello.config.MyDataSourceValueConfig;
+import hello.config.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Import;
 
-@Import(MyDataSourceConfigV2.class)
 //@Import(MyDataSourceConfigV1.class)
 //@Import(MyDataSourceValueConfig.class)
 //@Import(MyDataSourceEnvConfig.class)
-//@ConfigurationPropertiesScan
+//@Import(MyDataSourceConfigV2.class)
+@Import(MyDataSourceConfigV3.class)
 @SpringBootApplication(scanBasePackages = "hello.datasource")
+@ConfigurationPropertiesScan
 public class ExternalReadApplication {
 
     public static void main(String[] args) {
