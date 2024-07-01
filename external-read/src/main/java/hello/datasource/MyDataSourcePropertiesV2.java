1c50246fffa08f9a222e32618f6abe8c3723719c
falsystack <falsystack@gmail.com>
2024-07-01 23:54:27 +0900

    feat: @ConfigurationProperties with constructor

---
+++ external-read/src/main/java/hello/datasource/MyDataSourcePropertiesV2.java
@@ -0,0 +1,33 @@
+package hello.datasource;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.bind.DefaultValue;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+@ConfigurationProperties("my.datasource")
+public class MyDataSourcePropertiesV2 {
+    private String url;
+    private String username;
+    private String password;
+    private Etc etc;
+
+    @Getter
+    public static class Etc {
+        private int maxConnection;
+        private Duration timeout;
+        private List<String> options;
+
+        public Etc(int maxConnection, Duration timeout, @DefaultValue("DEFAULT") List<String> options) {
+            this.maxConnection = maxConnection;
+            this.timeout = timeout;
+            this.options = options;
+        }
+    }
+}
