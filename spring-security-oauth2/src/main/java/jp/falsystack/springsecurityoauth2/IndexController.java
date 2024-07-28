eb03c089f968e334b433546808a182bdeb2cf3a2
falsystack <falsystack@gmail.com>
2024-07-28 17:15:03 +0900

    feat: Custom login page

--- spring-security-oauth2/src/main/java/jp/falsystack/springsecurityoauth2/IndexController.java
+++ spring-security-oauth2/src/main/java/jp/falsystack/springsecurityoauth2/IndexController.java
@@ -1,13 +1,24 @@
 package jp.falsystack.springsecurityoauth2;
 
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class IndexController {
 
+  private final ClientRegistrationRepository clientRegistrationRepository;
+
+  public IndexController(ClientRegistrationRepository clientRegistrationRepository) {
+    this.clientRegistrationRepository = clientRegistrationRepository;
+  }
+
   @GetMapping("/")
   public String index() {
+    var clientRegistration = clientRegistrationRepository.findByRegistrationId("keycloak");
+
+    System.out.println("clientRegistration.getClientId() = " + clientRegistration.getClientId());
+
     return "Hello World!";
   }
 }
