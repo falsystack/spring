635c392ddfe278f912e6fbe4b92a3b5ee4fe5e20
falsystack <falsystack@gmail.com>
2024-08-14 08:05:16 +0900

    oauth2user & oidcuser

--- spring-security-oauth2/src/main/java/jp/falsystack/springsecurityoauth2/OAuth2ClientConfig.java
+++ spring-security-oauth2/src/main/java/jp/falsystack/springsecurityoauth2/OAuth2ClientConfig.java
@@ -4,10 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.registration.ClientRegistrations;
-import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -21,24 +17,25 @@ public class OAuth2ClientConfig {
 //                .requestMatchers("/loginPage")
 //                .permitAll()
                 .anyRequest()
-                .authenticated());
+                    .permitAll());
+//                .authenticated());
 //    http.oauth2Login(oauth2 -> oauth2.loginPage("/loginPage"));
     http.oauth2Login(Customizer.withDefaults());
     return http.build();
   }
 
-  @Bean
-  public ClientRegistrationRepository clientRegistrationRepository() {
-    return new InMemoryClientRegistrationRepository(keycloakClientRegistration());
-  }
-
-  private ClientRegistration keycloakClientRegistration() {
-    return ClientRegistrations.fromIssuerLocation("http://localhost:8080/realms/OAuth2")
-        .registrationId("keycloak")
-        .clientId("oauth2-client-app")
-        .clientSecret("aTTEYbLxElXEkebDcefJ2LYThRHwM5pf")
-        .redirectUri("http://localhost:8081/login/oauth2/code/keycloak")
-        .build();
-  }
+//  @Bean
+//  public ClientRegistrationRepository clientRegistrationRepository() {
+//    return new InMemoryClientRegistrationRepository(keycloakClientRegistration());
+//  }
+//
+//  private ClientRegistration keycloakClientRegistration() {
+//    return ClientRegistrations.fromIssuerLocation("http://localhost:8080/realms/OAuth2")
+//        .registrationId("keycloak")
+//        .clientId("oauth2-client-app")
+//        .clientSecret("aTTEYbLxElXEkebDcefJ2LYThRHwM5pf")
+//        .redirectUri("http://localhost:8081/login/oauth2/code/keycloak")
+//        .build();
+//  }
 
 }
