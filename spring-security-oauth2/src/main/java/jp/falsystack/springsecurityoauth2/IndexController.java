b8317c0232f61668347442eed46934a5db06ee35
falsystack <falsystack@gmail.com>
2024-08-16 07:25:56 +0900

    authentication users

--- spring-security-oauth2/src/main/java/jp/falsystack/springsecurityoauth2/IndexController.java
+++ spring-security-oauth2/src/main/java/jp/falsystack/springsecurityoauth2/IndexController.java
@@ -1,59 +1,43 @@
 package jp.falsystack.springsecurityoauth2;
 
-import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
-import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
-import org.springframework.security.oauth2.core.OAuth2AccessToken;
-import org.springframework.security.oauth2.core.oidc.IdTokenClaimNames;
-import org.springframework.security.oauth2.core.oidc.OidcIdToken;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.Instant;
-import java.util.HashMap;
-import java.util.Map;
-
 @RestController
 public class IndexController {
 
-  private final ClientRegistrationRepository clientRegistrationRepository;
+    private final ClientRegistrationRepository clientRegistrationRepository;
 
-  public IndexController(ClientRegistrationRepository clientRegistrationRepository) {
-    this.clientRegistrationRepository = clientRegistrationRepository;
-  }
+    public IndexController(ClientRegistrationRepository clientRegistrationRepository) {
+        this.clientRegistrationRepository = clientRegistrationRepository;
+    }
 
-  @GetMapping("/user")
-  public OAuth2User user(String accessToken) {
-    ClientRegistration clientRegistration = clientRegistrationRepository.findByRegistrationId("keycloak");
-    OAuth2AccessToken oAuth2AccessToken = new OAuth2AccessToken(OAuth2AccessToken.TokenType.BEARER, accessToken, Instant.now(), Instant.MAX);
+    @GetMapping("/user")
+    public OAuth2User user(Authentication authentication) {
+        // OAuth2AuthenticationToken 을 가져오는 두 가지 방법
+        OAuth2AuthenticationToken authenticationToken1 = (OAuth2AuthenticationToken) authentication;
+        OAuth2AuthenticationToken authenticationToken2 = (OAuth2AuthenticationToken) SecurityContextHolder.getContext().getAuthentication();
 
-    OAuth2UserRequest oAuth2UserRequest = new OAuth2UserRequest(clientRegistration, oAuth2AccessToken);
-    DefaultOAuth2UserService defaultOAuth2UserService = new DefaultOAuth2UserService();
-    OAuth2User oAuth2User = defaultOAuth2UserService.loadUser(oAuth2UserRequest);
+        OAuth2User oauth2User = authenticationToken1.getPrincipal();
+        return oauth2User;
+    }
 
-    return oAuth2User;
-  }
+    @GetMapping("/oauth2User")
+    public OAuth2User oauth2User(@AuthenticationPrincipal OAuth2User oauth2User) {
+        System.out.println("oauth2User = " + oauth2User);
+        return oauth2User;
+    }
 
-  @GetMapping("/oidc")
-  public OAuth2User oidc(String accessToken, String idToken) {
-    ClientRegistration clientRegistration = clientRegistrationRepository.findByRegistrationId("keycloak");
-    OAuth2AccessToken oAuth2AccessToken = new OAuth2AccessToken(OAuth2AccessToken.TokenType.BEARER, accessToken, Instant.now(), Instant.MAX);
-
-    Map<String, Object> idTokenClaims = new HashMap<>();
-    idTokenClaims.put(IdTokenClaimNames.ISS, "http://localhost:8080/realms/oauth2");
-    idTokenClaims.put(IdTokenClaimNames.SUB, "OIDC0");
-    idTokenClaims.put("preferred_username", "user");
-
-    OidcIdToken oidcIdToken = new OidcIdToken(idToken, Instant.now(), Instant.MAX, idTokenClaims);
-
-    OidcUserRequest oidcUserRequest = new OidcUserRequest(clientRegistration, oAuth2AccessToken, oidcIdToken);
-    OidcUserService oidcUserService = new OidcUserService();
-    OAuth2User oAuth2User = oidcUserService.loadUser(oidcUserRequest);
-
-    return oAuth2User;
-  }
+    @GetMapping("/oidcUser")
+    public OAuth2User oidcUser(@AuthenticationPrincipal OidcUser oidcUser) {
+        System.out.println("oidcUser = " + oidcUser);
+        return oidcUser;
+    }
 }
