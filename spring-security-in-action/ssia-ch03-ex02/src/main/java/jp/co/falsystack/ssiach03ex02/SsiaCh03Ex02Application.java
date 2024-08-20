8677c952ad9fcdc4dfe99e3d3d04079a764a81e1
falsystack <falsystack@gmail.com>
2024-08-21 08:46:17 +0900

    ssia-ch03-ex02 done

---
+++ spring-security-in-action/ssia-ch03-ex02/src/main/java/jp/co/falsystack/ssiach03ex02/SsiaCh03Ex02Application.java
@@ -0,0 +1,13 @@
+package jp.co.falsystack.ssiach03ex02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsiaCh03Ex02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsiaCh03Ex02Application.class, args);
+    }
+
+}
---
+++ spring-security-in-action/ssia-ch03-ex02/src/test/java/jp/co/falsystack/ssiach03ex02/SsiaCh03Ex02ApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.falsystack.ssiach03ex02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsiaCh03Ex02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
