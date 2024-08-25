1cbe2382c17f4a2e2bc16df61d9e516199eda531
falsystack <falsystack@gmail.com>
2024-08-26 07:36:20 +0900

    authority & role

---
+++ spring-security-in-action/ssia-ch07-ex01/src/main/java/jp/co/falsystack/ssiach07ex01/SsiaCh07Ex01Application.java
@@ -0,0 +1,13 @@
+package jp.co.falsystack.ssiach07ex01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsiaCh07Ex01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsiaCh07Ex01Application.class, args);
+    }
+
+}
---
+++ spring-security-in-action/ssia-ch07-ex01/src/test/java/jp/co/falsystack/ssiach07ex01/SsiaCh07Ex01ApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.falsystack.ssiach07ex01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsiaCh07Ex01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
