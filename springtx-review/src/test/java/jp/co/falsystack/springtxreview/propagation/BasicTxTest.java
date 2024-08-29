f5ac8e1cc112cb7e6e84056a29d4eaeb06304fec
falsystack <falsystack@gmail.com>
2024-08-29 22:59:42 +0900

    transaction propagation

---
+++ springtx-review/src/test/java/jp/co/falsystack/springtxreview/propagation/BasicTxTest.java
@@ -0,0 +1,99 @@
+package jp.co.falsystack.springtxreview.propagation;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+
+import javax.sql.DataSource;
+
+@Slf4j
+@SpringBootTest
+public class BasicTxTest {
+
+    @Autowired
+    PlatformTransactionManager txManager;
+
+    @Test
+    void commit() {
+        log.info("트랜잭션 시작");
+        var status = txManager.getTransaction(new DefaultTransactionDefinition());
+
+        log.info("트랜잭션 커밋 시작");
+        txManager.commit(status);
+        log.info("트랜잭션 커밋 완료");
+    }
+
+    @Test
+    void rollback() {
+        log.info("트랜잭션 시작");
+        var status = txManager.getTransaction(new DefaultTransactionDefinition());
+
+        log.info("트랜잭션 롤백 시작");
+        txManager.rollback(status);
+        log.info("트랜잭션 롤백 완료");
+    }
+
+    @Test
+    void double_commit() {
+        log.info("트랜잭션1 시작");
+        var tx1 = txManager.getTransaction(new DefaultTransactionDefinition());
+        log.info("트랜잭션1 커밋 시작");
+        txManager.commit(tx1);
+        log.info("트랜잭션1 커밋 완료");
+
+        log.info("트랜잭션2 시작");
+        var tx2 = txManager.getTransaction(new DefaultTransactionDefinition());
+        log.info("트랜잭션2 커밋 시작");
+        txManager.commit(tx2);
+        log.info("트랜잭션2 커밋 완료");
+    }
+
+    @Test
+    void double_commit_rollback() {
+        log.info("트랜잭션1 시작");
+        var tx1 = txManager.getTransaction(new DefaultTransactionDefinition());
+        log.info("트랜잭션1 커밋 시작");
+        txManager.commit(tx1);
+        log.info("트랜잭션1 커밋 완료");
+
+        log.info("트랜잭션2 시작");
+        var tx2 = txManager.getTransaction(new DefaultTransactionDefinition());
+        log.info("트랜잭션2 롤백 시작");
+        txManager.rollback(tx2);
+        log.info("트랜잭션2 롤백 완료");
+    }
+
+    @Test
+    void inner_commit() {
+        log.info("외부 트랜잭션 시작");
+        var outer = txManager.getTransaction(new DefaultTransactionDefinition());
+        log.info("outer.isNewTransaction() = {}", outer.isNewTransaction());
+
+        log.info("내부 트랜잭션 시작");
+        var inner = txManager.getTransaction(new DefaultTransactionDefinition());
+        log.info("inner.isNewTransaction() = {}", inner.isNewTransaction());
+        log.info("내부 트랜잭션 커밋");
+        txManager.commit(inner);
+
+        log.info("외부 트랜잭션 커밋");
+        txManager.commit(outer);
+    }
+
+    @TestConfiguration
+    static class Config {
+
+        @Bean
+        public PlatformTransactionManager transactionManager(DataSource dataSource) {
+            return new DataSourceTransactionManager(dataSource);
+        }
+
+
+    }
+}
