04957379932be798d3caa3182c762f3116670196
falsystack <falsystack@gmail.com>
2024-09-03 08:22:11 +0900

    transaction propagation requires_new

--- springtx-review/src/test/java/jp/co/falsystack/springtxreview/propagation/BasicTxTest.java
+++ springtx-review/src/test/java/jp/co/falsystack/springtxreview/propagation/BasicTxTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.DefaultTransactionDefinition;
 
@@ -107,6 +108,23 @@ public class BasicTxTest {
 
         log.info("내부 트랜잭션 시작");
         var inner = txManager.getTransaction(new DefaultTransactionDefinition());
+        log.info("내부 트랜잭션 롤백");
+        txManager.rollback(inner);
+
+        log.info("외부 트랜잭션 커밋");
+        txManager.commit(outer);
+    }
+
+    @Test
+    void inner_rollback_requires_new() {
+        log.info("외부 트랜잭션 시작");
+        var outer = txManager.getTransaction(new DefaultTransactionDefinition());
+        log.info("outer.isNewTransaction() = {}", outer.isNewTransaction()); // true
+
+        log.info("내부 트랜잭션 시작");
+        var inner = txManager.getTransaction(new DefaultTransactionDefinition(TransactionDefinition.PROPAGATION_REQUIRES_NEW));
+        log.info("inner.isNewTransaction() = {}", inner.isNewTransaction()); // true
+
         log.info("내부 트랜잭션 롤백");
         txManager.rollback(inner);
 
