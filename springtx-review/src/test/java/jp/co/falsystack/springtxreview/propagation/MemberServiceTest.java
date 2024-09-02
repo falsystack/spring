9a4b68ffcac916df3dfc76071b266f6b9f856f82
falsystack <falsystack@gmail.com>
2024-09-03 08:55:26 +0900

    outerTxOff_success

---
+++ springtx-review/src/test/java/jp/co/falsystack/springtxreview/propagation/MemberServiceTest.java
@@ -0,0 +1,40 @@
+package jp.co.falsystack.springtxreview.propagation;
+
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@Slf4j
+@SpringBootTest
+class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+    @Autowired
+    MemberRepository memberRepository;
+    @Autowired
+    LogRepository logRepository;
+
+    /**
+     * memberService        @Transactional: OFF
+     * memberRepository     @Transactional: ON
+     * logRepository        @Transactional: ON
+     */
+    @Test
+    void outerTxOff_success() {
+        // given
+        var username = "outerTxOff_success";
+
+        // when
+        memberService.joinV1(username);
+
+        // then
+        assertThat(memberRepository.find(username).isPresent()).isTrue();
+        assertThat(logRepository.find(username).isPresent()).isTrue();
+    }
+}
