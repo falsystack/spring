0e32b2b30bb86b000aa2736a43a117355f9ef1bc
falsystack <falsystack@gmail.com>
2024-09-09 07:03:43 +0900

    start spring security inflearn

---
+++ spring-security-inflearn/sso2-client/src/main/java/jp/co/falsystack/sso2client/Sso2ClientApplication.java
@@ -0,0 +1,13 @@
+package jp.co.falsystack.sso2client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sso2ClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sso2ClientApplication.class, args);
+    }
+
+}
---
+++ spring-security-inflearn/sso2-client/src/test/java/jp/co/falsystack/sso2client/Sso2ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.falsystack.sso2client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sso2ClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
