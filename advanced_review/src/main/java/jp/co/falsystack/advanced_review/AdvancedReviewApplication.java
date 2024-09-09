f25127b8990a34f35ccfaa4deb365de112a098ee
falsystack <falsystack@gmail.com>
2024-09-10 07:29:23 +0900

    advanced_review start

---
+++ advanced_review/src/main/java/jp/co/falsystack/advanced_review/AdvancedReviewApplication.java
@@ -0,0 +1,13 @@
+package jp.co.falsystack.advanced_review;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancedReviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdvancedReviewApplication.class, args);
+    }
+
+}
---
+++ advanced_review/src/test/java/jp/co/falsystack/advanced_review/AdvancedReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.falsystack.advanced_review;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvancedReviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
