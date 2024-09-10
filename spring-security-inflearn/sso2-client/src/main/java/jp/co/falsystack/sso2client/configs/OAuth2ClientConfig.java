dac73a0a5e613847576df7e4c0b42b35909fcf68
falsystack <falsystack@gmail.com>
2024-09-10 20:30:01 +0900

    oauth2-client : custom oauth2 login page

--- spring-security-inflearn/sso2-client/src/main/java/jp/co/falsystack/sso2client/configs/OAuth2ClientConfig.java
+++ spring-security-inflearn/sso2-client/src/main/java/jp/co/falsystack/sso2client/configs/OAuth2ClientConfig.java
@@ -2,26 +2,21 @@ package jp.co.falsystack.sso2client.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.registration.ClientRegistrations;
-import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
+@EnableWebSecurity
 @Configuration
 public class OAuth2ClientConfig {
 
     @Bean
-    protected ClientRegistrationRepository clientRegistrationRepository() {
-        return new InMemoryClientRegistrationRepository(keycloakClientRegistration());
-    }
+    protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(httpRequest -> httpRequest
+                .requestMatchers("/login").permitAll()
+                .anyRequest().authenticated());
+        http.oauth2Login(oauth -> oauth.loginPage("/login"));
 
-    private ClientRegistration keycloakClientRegistration() {
-        return ClientRegistrations.fromIssuerLocation("http://localhost:8080/realms/oauth2")
-                .registrationId("keycloak")
-                .clientId("oauth2-client-app")
-                .clientSecret("eoOgG57DSNmd1fD8q7vhCz94Ocv6DbxW")
-                .redirectUri("http://localhost:8081/login/oauth2/code/keycloak")
-                .scope("openid", "profile", "email") // keycloak 버전이 업데이트 되어서 scope, 특히 openid 가 없으면 403 에러가 난다
-                .build();
+        return http.build();
     }
 }
--- spring-security-inflearn/sso2-client/src/main/java/jp/co/falsystack/sso2client/controllers/IndexController.java
+++ spring-security-inflearn/sso2-client/src/main/java/jp/co/falsystack/sso2client/controllers/IndexController.java
@@ -1,27 +1,26 @@
 package jp.co.falsystack.sso2client.controllers;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
 public class IndexController {
 
-    private final ClientRegistrationRepository clientRegistrationRepository;
+//    private final ClientRegistrationRepository clientRegistrationRepository;
 
 //    @GetMapping("/")
-//    public String index() {
-//        return "index";
-//    }
+    public String index() {
+        return "index";
+    }
 
-    @GetMapping("/")
+//    @GetMapping("/")
     public String getClientRegistrations() {
-        var clientRegistration = clientRegistrationRepository.findByRegistrationId("keycloak");
-
-        clientRegistration.getClientId();
-        return clientRegistration.getRegistrationId();
+//        var clientRegistration = clientRegistrationRepository.findByRegistrationId("keycloak");
+//
+//        clientRegistration.getClientId();
+//        return clientRegistration.getRegistrationId();
+        return "";
     }
 }
