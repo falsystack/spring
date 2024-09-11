58d771c701dfce6b7ee34d5c37f4948289517cb6
falsystack <falsystack@gmail.com>
2024-09-11 09:53:02 +0900

    oauth2-client : oidc logout

--- spring-security-inflearn/sso2-client/src/main/java/jp/co/falsystack/sso2client/configs/OAuth2ClientConfig.java
+++ spring-security-inflearn/sso2-client/src/main/java/jp/co/falsystack/sso2client/configs/OAuth2ClientConfig.java
@@ -1,24 +1,42 @@
 package jp.co.falsystack.sso2client.configs;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
+@RequiredArgsConstructor
 @EnableWebSecurity
 @Configuration
 public class OAuth2ClientConfig {
 
+    private final ClientRegistrationRepository clientRegistrationRepository;
+
     @Bean
     protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(httpRequest -> httpRequest.anyRequest().permitAll());
-//                .requestMatchers("/login","/user").permitAll()
-//                .requestMatchers(HttpMethod.POST,"/user").permitAll()
-//                .anyRequest().authenticated());
-//        http.oauth2Login(oauth -> oauth.loginPage("/login"));
+        http.authorizeHttpRequests(httpRequest -> httpRequest.anyRequest().authenticated());
+        http.oauth2Login(Customizer.withDefaults());
+        http.oidcLogout();
+        http.logout(logout ->
+                logout.logoutSuccessHandler(oidcLogoutSuccessHandler())
+                        .invalidateHttpSession(true)
+                        .clearAuthentication(true)
+                        .deleteCookies("JSESSIONID")
+        );
 
         return http.build();
+    }
+
+    private LogoutSuccessHandler oidcLogoutSuccessHandler() {
+        var successHandler = new OidcClientInitiatedLogoutSuccessHandler(clientRegistrationRepository);
+        successHandler.setPostLogoutRedirectUri("http://localhost:8080/login");
+        return successHandler;
     }
 }
