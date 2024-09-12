398aea7d9b60479c6fef8d1fd5aacdf60bf64d61
falsystack <falsystack@gmail.com>
2024-09-12 09:04:28 +0900

    sso2-resource-server: start

---
+++ spring-security-inflearn/sso2-resource-server/src/main/java/jp/co/falsystack/sso2resourceserver/Sso2ResourceServerApplication.java
@@ -0,0 +1,13 @@
+package jp.co.falsystack.sso2resourceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sso2ResourceServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sso2ResourceServerApplication.class, args);
+    }
+
+}
---
+++ spring-security-inflearn/sso2-resource-server/src/test/java/jp/co/falsystack/sso2resourceserver/Sso2ResourceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.falsystack.sso2resourceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sso2ResourceServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
