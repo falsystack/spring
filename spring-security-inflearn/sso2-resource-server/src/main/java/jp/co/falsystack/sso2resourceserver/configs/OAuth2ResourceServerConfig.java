600e3a484c664aad5c147c8e6ea951d9bacb5b4c
falsystack <falsystack@gmail.com>
2024-09-14 16:45:04 +0900

    sso2-resource-server: JwtDecoder

--- spring-security-inflearn/sso2-resource-server/src/main/java/jp/co/falsystack/sso2resourceserver/configs/OAuth2ResourceServerConfig.java
+++ spring-security-inflearn/sso2-resource-server/src/main/java/jp/co/falsystack/sso2resourceserver/configs/OAuth2ResourceServerConfig.java
@@ -1,13 +1,20 @@
 package jp.co.falsystack.sso2resourceserver.configs;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
 import org.springframework.security.web.SecurityFilterChain;
 
+@RequiredArgsConstructor
 @Configuration
 public class OAuth2ResourceServerConfig {
+
+    private final OAuth2ResourceServerProperties properties;
 
     @Bean
     protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -16,5 +23,10 @@ public class OAuth2ResourceServerConfig {
         return http.build();
     }
 
+    @Bean
+    protected JwtDecoder jwtDecoder() {
+        // return JwtDecoders.fromIssuerLocation(properties.getJwt().getIssuerUri());
+        return JwtDecoders.fromOidcIssuerLocation(properties.getJwt().getIssuerUri());
+    }
 
 }
