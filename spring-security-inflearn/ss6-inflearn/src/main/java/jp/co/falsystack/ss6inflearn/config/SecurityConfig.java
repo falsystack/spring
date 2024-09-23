3e6bac4956bc3352a94fa07f68eb8bf5cf20e98d
falsystack <falsystack@gmail.com>
2024-09-24 07:46:42 +0900

    spring security 6 : httpBasic()

--- spring-security-inflearn/ss6-inflearn/src/main/java/jp/co/falsystack/ss6inflearn/config/SecurityConfig.java
+++ spring-security-inflearn/ss6-inflearn/src/main/java/jp/co/falsystack/ss6inflearn/config/SecurityConfig.java
@@ -13,26 +13,9 @@ public class SecurityConfig {
     @Bean
     protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
-                .formLogin(form ->
-                        form
-//                                .loginPage("/login")
-                                .loginProcessingUrl("/loginProc")
-                                .defaultSuccessUrl("/", true) // true 이면 무조건 지정한 url 로 리디렉트 되지만 false 일 경우 이전 요청 url 로 리디렉트 될 수 있다
-                                .failureUrl("/failed")
-                                .usernameParameter("username")
-                                .passwordParameter("password")
-                                // defaultSuccessUrl 의 설정을 override 한다.
-                                .successHandler((request, response, authentication) -> {
-                                    System.out.println("authentication = " + authentication.getPrincipal().toString());
-                                    response.sendRedirect("/home");
-                                })
-                                // failureUrl 의 설정을 override 한다.
-                                .failureHandler((request, response, exception) -> {
-                                    System.out.println("exception = " + exception.getMessage());
-                                    response.sendRedirect("/login");
-                                })
-                                .permitAll()
-                );
+                .httpBasic(basic -> {
+                    basic.authenticationEntryPoint(new CustomAuthenticationEntryPoint());
+                });
 
         return http.build();
     }
