1eb21028ab012a54e300243607f1a742f604f9ab
falsystack <falsystack@gmail.com>
2024-09-26 08:39:57 +0900

    msa reset

--- msa/catalog-service/src/main/java/jp/falsystack/catalogservice/repository/CatalogRepository.java
+++
@@ -1,8 +0,0 @@
-package jp.falsystack.catalogservice.repository;
-
-import jp.falsystack.catalogservice.entity.Catalog;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CatalogRepository extends JpaRepository<Catalog, Long> {
-    Catalog findByProductId(String productId);
-}
--- msa/catalog-service/src/main/java/jp/falsystack/catalogservice/service/CatalogService.java
+++
@@ -1,9 +0,0 @@
-package jp.falsystack.catalogservice.service;
-
-import jp.falsystack.catalogservice.vo.ResponseCatalog;
-
-import java.util.List;
-
-public interface CatalogService {
-    List<ResponseCatalog> getAllCatalogs();
-}
