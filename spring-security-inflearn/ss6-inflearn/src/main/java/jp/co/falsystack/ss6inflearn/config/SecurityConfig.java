7fc1681cfb820119c79f69511e6894d8258c6f5d
falsystack <falsystack@gmail.com>
2024-09-28 11:14:31 +0900

    spring security 6 : requestCache, savedRequest

--- spring-security-inflearn/ss6-inflearn/src/main/java/jp/co/falsystack/ss6inflearn/config/SecurityConfig.java
+++ spring-security-inflearn/ss6-inflearn/src/main/java/jp/co/falsystack/ss6inflearn/config/SecurityConfig.java
@@ -2,15 +2,13 @@ package jp.co.falsystack.ss6inflearn.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
 
 @EnableWebSecurity
 @Configuration
@@ -18,31 +16,22 @@ public class SecurityConfig {
 
     @Bean
     protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        var requestCache = new HttpSessionRequestCache();
+        requestCache.setMatchingRequestParameterName("seoul");
+
         http.authorizeHttpRequests(auth -> auth
                         .requestMatchers("/anonymous").hasRole("GUEST")
                         .requestMatchers("/logoutSuccess", "/anonymousContext", "/authentication").permitAll()
                         .anyRequest().authenticated())
-                .formLogin(Customizer.withDefaults())
-//                .csrf(csrf -> csrf.disable())
-                .logout(logout -> logout
-//                        .logoutUrl("/logoutProc")
-//                        .logoutRequestMatcher(new AntPathRequestMatcher("/logoutProc", "POST"))
-                        .logoutSuccessUrl("/logoutSuccess")
-                        .logoutSuccessHandler((request, response, authentication) -> {
-                            // 복잡한 설정이 필요할 경우 logoutSuccessHandler 를 사용
-                            System.out.println("logoutSuccessHandler");
-                            response.sendRedirect("/logoutSuccess");
-                        })
-                        .deleteCookies("JSESSIONID", "remember-me")
-                        .invalidateHttpSession(true)
-                        .clearAuthentication(true)
-                        .addLogoutHandler((request, response, authentication) -> {
-                            System.out.println("addLogoutHandler");
-                            request.getSession().invalidate();
-                            SecurityContextHolder.getContext().setAuthentication(null);
-                            SecurityContextHolder.getContextHolderStrategy().clearContext();
-                        })
-                );
+                .formLogin(form -> {
+                    form.successHandler((request, response, authentication) -> {
+                        // 인증전의 저장된 요청을 가져올 수 있다.
+                        var savedRequest = requestCache.getRequest(request, response);
+                        response.sendRedirect(savedRequest.getRedirectUrl());
+                    });
+                })
+                .requestCache(cache -> cache
+                        .requestCache(requestCache));
 
         return http.build();
     }
