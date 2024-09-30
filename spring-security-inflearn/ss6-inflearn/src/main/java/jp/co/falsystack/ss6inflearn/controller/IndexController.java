605eeadd77556d0b86f55398f97f122c0553883b
falsystack <falsystack@gmail.com>
2024-10-01 00:03:50 +0900

    spring security 6 : SecurityContextHolder.getContextHolderStrategy()

--- spring-security-inflearn/ss6-inflearn/src/main/java/jp/co/falsystack/ss6inflearn/controller/IndexController.java
+++ spring-security-inflearn/ss6-inflearn/src/main/java/jp/co/falsystack/ss6inflearn/controller/IndexController.java
@@ -4,6 +4,7 @@ import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.CurrentSecurityContext;
 import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,9 @@ public class IndexController {
 
     @GetMapping("/")
     public String index() {
+        var securityContext = SecurityContextHolder.getContextHolderStrategy().getContext();
+        var authentication = securityContext.getAuthentication();
+        System.out.println("authentication = " + authentication);
         return "index";
     }
 
