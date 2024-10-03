7d5c33e9e1f229697a01445c0f7d8b4ebc169ba3
falsystack <falsystack@gmail.com>
2024-10-04 08:17:53 +0900

    msa : gateway

---
+++ msa/apigatewayservice/src/main/java/jp/co/falsystack/apigatewayservice/confg/FilterConfig.java
@@ -0,0 +1,27 @@
+package jp.co.falsystack.apigatewayservice.confg;
+
+//@Configuration
+public class FilterConfig {
+
+//    @Bean
+//    protected RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+//        return builder.routes()
+//                .route(r -> r
+//                        .path("/first-service/**")
+//                        .filters(f -> f
+//                                .addRequestHeader("first-request", "first-request-header")
+//                                .addResponseHeader("first-response", "first-response-header")
+//                        )
+//                        .uri("http://localhost:8081")
+//                )
+//                .route(r -> r
+//                        .path("/second-service/**")
+//                        .filters(f -> f
+//                                .addRequestHeader("second-request", "second-request-header")
+//                                .addResponseHeader("second-response", "second-response-header")
+//                        )
+//                        .uri("http://localhost:8082")
+//                )
+//                .build();
+//    }
+}
---
+++ msa/firstservice/src/test/java/jp/co/falsystack/firstservice/FirstserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.falsystack.firstservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
