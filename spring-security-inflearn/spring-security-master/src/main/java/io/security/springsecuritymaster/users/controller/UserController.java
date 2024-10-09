49012a650ab4ff111b3808702f73a95f6524601d
falsystack <falsystack@gmail.com>
2024-10-09 23:10:56 +0900

    ss6 : practice1 signup

---
+++ spring-security-inflearn/spring-security-master/src/main/java/io/security/springsecuritymaster/users/controller/UserController.java
@@ -0,0 +1,32 @@
+package io.security.springsecuritymaster.users.controller;
+
+import io.security.springsecuritymaster.domain.dto.AccountDto;
+import io.security.springsecuritymaster.domain.entity.Account;
+import io.security.springsecuritymaster.users.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Map;
+
+@Controller
+@RequiredArgsConstructor
+public class UserController {
+
+    private final PasswordEncoder passwordEncoder;
+    private final UserService userService;
+
+    @PostMapping("signup")
+    public String signup(AccountDto accountDto) {
+        var mapper = new ModelMapper();
+        var account = mapper.map(accountDto, Account.class);
+        account.setPassword(passwordEncoder.encode(accountDto.getPassword()));
+
+        userService.createUser(account);
+
+        return "redirect:/";
+    }
+
+}
