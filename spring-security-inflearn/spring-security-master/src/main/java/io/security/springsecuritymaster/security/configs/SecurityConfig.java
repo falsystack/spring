f3b4df7892c357fec888f9d204597914c20be086
falsystack <falsystack@gmail.com>
2024-10-11 08:57:45 +0900

    ss6 : practice1 authentication failure handler

--- spring-security-inflearn/spring-security-master/src/main/java/io/security/springsecuritymaster/security/configs/SecurityConfig.java
+++ spring-security-inflearn/spring-security-master/src/main/java/io/security/springsecuritymaster/security/configs/SecurityConfig.java
@@ -17,6 +17,7 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.WebAuthenticationDetails;
 
@@ -29,18 +30,20 @@ public class SecurityConfig {
     private final FormAuthenticationProvider formAuthenticationProvider;
     private final AuthenticationDetailsSource<HttpServletRequest, WebAuthenticationDetails> authenticationDetailsSource;
     private final AuthenticationSuccessHandler authenticationSuccessHandler;
+    private final AuthenticationFailureHandler authenticationFailureHandler;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/css/**", "/images/**", "/js/**", "/favicon.*", "/*/icon-*").permitAll()
-                        .requestMatchers("/", "/signup").permitAll()
+                        .requestMatchers("/", "/signup", "/login*").permitAll()
                         .anyRequest().authenticated())
                 .formLogin(form -> form
                         .loginPage("/login").permitAll()
                         .authenticationDetailsSource(authenticationDetailsSource)
                         .successHandler(authenticationSuccessHandler)
+                        .failureHandler(authenticationFailureHandler)
                 )
                 .authenticationProvider(formAuthenticationProvider)
 //                .userDetailsService(formUserDetailService)
--- spring-security-inflearn/spring-security-master/src/main/java/io/security/springsecuritymaster/users/controller/LoginController.java
+++ spring-security-inflearn/spring-security-master/src/main/java/io/security/springsecuritymaster/users/controller/LoginController.java
@@ -2,16 +2,23 @@ package io.security.springsecuritymaster.users.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.Data;
+import lombok.Getter;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
 public class LoginController {
 
     @GetMapping("/login")
-    public String login() {
+    public String login(@ModelAttribute LoginParam params) {
+//    public String login(@RequestParam String error,
+//                        @RequestParam String exception, Model model) {
+//        model.addAttribute("error", error);
+//        model.addAttribute("exception", exception);
         return "login/login";
     }
 
@@ -28,5 +35,12 @@ public class LoginController {
             new SecurityContextLogoutHandler().logout(request, response, authentication);
         }
         return "redirect:/login";
+    }
+
+
+    @Data
+    private static class LoginParam {
+        private String error;
+        private String exception;
     }
 }
